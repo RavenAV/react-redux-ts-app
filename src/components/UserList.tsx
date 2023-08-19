5dc9120c6ca955ed82e205a51a24e8539e3dc07e
RavenAV <anubis.27007@mail.ru>
2023-08-19 22:50:01 +0300

    Add first reduser, typifiation and combine reducer

---
+++ src/components/UserList.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {useSelector} from "react-redux";
+
+export const UserList: React.FC = () => {
+    const state = useSelector(state => state)
+    console.log(state)
+    return (
+        <div>
+
+        </div>
+    )
+}
---
+++ src/store/reducers/userReducer.ts
@@ -0,0 +1,22 @@
+import {UserAction, UserActionTypes, UserState} from "../../types/todo";
+
+// описываем тип состояние
+const initialState : UserState = {
+    users: [],
+    loading: false,
+    error: null
+}
+
+// указываем, что редьюсер такого типа должен возвращать объект
+export const userReducer = (state = initialState, action: UserAction) : UserState => {
+    switch (action.type) {
+        case UserActionTypes.FETCH_USERS:
+            return { loading: true, error: null, users: [] }
+        case UserActionTypes.FETCH_USERS_SUCCESS:
+            return { loading: false, error: null, users: action.payload }
+        case UserActionTypes.FETCH_USERS_ERROR:
+            return { loading: false, error: action.payload , users: [] }
+        default:
+            return state
+    }
+}
